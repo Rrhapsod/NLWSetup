205ecc0df36e70dd10aeed9435aa38382c040eed
Rrhapsod <renanrondon@live.com>
2023-01-24 23:11:19 -0300

    Aula 2 completa

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./index.html",
-  ],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -15,9 +11,6 @@ module.exports = {
         semibold: "Inter_600SemiBold",
         bold: "Inter_700Bold",
         extrabold: "Inter_800ExtraBold",
-      },
-      gridTemplateRows: {
-        7: "repeat(7, minmax(0, 1fr))",
       },
     },
   },
--- web/src/components/Habit.tsx
+++
@@ -1,5 +0,0 @@
-export function Habit(){
-  return (
-    <div className="bg-zinc-900" >Nossa</div>
-  )
-}
